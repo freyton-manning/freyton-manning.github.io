bd758ec3bf7f8ab264d8fe9b3c9bc23acd5a025a
Freya <Freya@ooyala.com>
2014-07-01 09:07:04 -0700

    this is my third commit

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 	//$('.carousel').carousel(function() {
   	//	interval: 200
 	//});
+	$('.watch-now').on('click', function (e) {
+		console.log('here');
+	    $('.carousel').carousel('pause');
+	    OO.Player.create('ooyalaplayer', 'NsNHRnbjrIhhwnEMJSG939wey4-c0PK7', {"autoplay":true}); 
+	});
+	//opposite click event 
+	$(document).click(function(e) {
+//check that e.target is not the watch now button 
+//
+        console.log('destroying');
+        OO.Player.destroy();
+	});
 });
+
+
+
