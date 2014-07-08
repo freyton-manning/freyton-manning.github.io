198a9b30f80bfa5f5f15cb781289a3ffbee9e6f9
Freya <Freya@ooyala.com>
2014-07-07 23:46:09 -0700

    added dynamic popover hover

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ $(document).ready(function() {
 	    OO.Player.create('ooyalaplayer', 'NsNHRnbjrIhhwnEMJSG939wey4-c0PK7', {"autoplay":true}); 
 	    $('#ooyalaplayer');
 	});
+
+	var $img2 = $('#img2');
+$img2.data('state', 'hover');
+
+var enterShow = function () {
+    if ($img2.data('state') === 'hover') {
+        $img2.popover('show');
+    }
+};
+var exitHide = function () {
+    if ($img2.data('state') === 'hover') {
+        $img2.popover('hide');
+    }
+};
+
+var clickToggle = function () {
+    if ($img2.data('state') === 'hover') {
+        $img2.data('state', 'pinned');
+    } else {
+        $img2.data('state', 'hover')
+        $img.popover('hover');
+    }
+};
+
+$img2.popover({trigger: 'manual'})
+    .on('mouseenter', enterShow)
+    .on('mouseleave', exitHide)
+    .on('click', clickToggle);
+
+
 	//opposite click event 
 	$(document).click(function(e) {
 //check that e.target is not the watch now button 
